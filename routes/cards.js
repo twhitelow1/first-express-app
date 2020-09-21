1ef459f7392dc4c0d782246a45661eaa5ec6d215
Todd Whitelow <todd@whitelow.tech>
2020-09-21 03:59:37 -0600

    update

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const data = require('../data/flashcardData.json')
+const { data } = require('../data/flashcardData.json')
+const { cards } = data;
 
 router.get('/', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grants tomb?" });
+  res.render('card', {
+    prompt: cards[0].question,
+    hint: cards[0].hint
+  });
 });
 
 module.exports = router;
