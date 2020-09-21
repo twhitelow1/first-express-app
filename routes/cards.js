2b0f041aba3f2caf06d2ef0ffe72f62cec625d13
Todd Whitelow <todd@whitelow.tech>
2020-09-21 03:53:06 -0600

    require data file

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-
+const data = require('../data/flashcardData.json')
 
 router.get('/', (req, res) => {
   res.render('card', { prompt: "Who is buried in Grants tomb?" });
