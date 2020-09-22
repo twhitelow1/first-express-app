33864871e15b769794f8984bffc08e406c2118dc
Todd Whitelow <todd@whitelow.tech>
2020-09-21 23:20:21 -0600

    fix styles

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 // Setup App
 const express = require('express');
 const app = express();
+// -------------------------------------
 // Middleware
+// -------------------------------------
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 
-//Use bodyparser
+// ----------------------------------------
+// Use bodyparser
+// ----------------------------------------
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 
+// ---------------------------------------
 app.set('view engine', 'pug');
 
+// ----------------------------------------
+// Routing
+// ----------------------------------------
 const mainRoutes = require('./routes');
 const cardRoutes = require('./routes/cards');
 
 app.use(mainRoutes);
 app.use('/cards', cardRoutes)
 
+// Error Handeling
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
@@ -28,6 +38,8 @@ app.use((err, req, res, next) => {
   res.status(err.status);
   res.render('error', err);
 });
+
+// Start Server
 
 app.listen(3000, () => {
   console.log('The application is running on locahost:3000!')
--- routes/cards.js
+++ routes/cards.js
@@ -3,23 +3,26 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json')
 const { cards } = data;
 
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardId}`)
+});
+
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
+
+  if (!side) {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
+  const name = req.cookies.username;
   const text = cards[id][side];
   const { hint } = cards[id];
   const sideToShow = (side === 'question' ? 'answer' : 'question')
-  const templateData = { text, id, sideToShow };
-
-  if (side === 'question') {
-    templateData.hint = hint;
-    templateData.link = "./1?side=answer";
-    templateData.linkText = "Answer"
-  } else if (side === 'answer') {
-    templateData.hint = hint;
-    templateData.link = "./1?side=question";
-    templateData.linkText = "Question"
-  }
+  const templateData = { text, id, sideToShow, name, side };
+  templateData.linkText = (side === 'question' ? 'Answer' : 'Question')
+  templateData.hint = hint;
 
   res.render('card', templateData);
 });
